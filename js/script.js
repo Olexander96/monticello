0b71a7bfb6d0c0b6f8560c1b218399f7660d58b2
Olexander96 <o.sheiko96@gmail.com>
2022-10-09 17:09:23 +0300

    completed header and hero section

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$('.hero__slider').slick({
+    arrows: false,
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    vertical: true,
+    autoplay:true,
+    autoplaySpeed: 5000,
+    verticalSwiping: true,
+});
